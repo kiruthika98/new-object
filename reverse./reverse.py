0c9b018b712f9e1eca146b9e6456b3ce0f6affa1
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-02-27 16:30:33 +0530

    reverse

---
+++ reverse./reverse.py
@@ -0,0 +1,9 @@
+
+def reverse(string):
+    string = "".join(reversed(string))
+    return string
+ s = "books"
+ print ("The original string  is : ",end="")
+print (s)
+ print ("The reversed string(using reversed) is : ",end="")
+print (reverse(s))
