93b1803fc3691ab5dc50b9e922b974d27572bbae
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-05-24 12:22:14 +0530

    new018

---
+++ new018.py
@@ -0,0 +1,10 @@
+n1,k1=map(int,input().split(' '))
+l1=list(map(int,input().split(' ')))
+k1=list(input().split(' '))
+m1=[]
+for i in range(len(k1)):
+    l1.append(i)
+    m1.append(max(l))
+    l1.remove(i)
+print(" ".join(str(x) for x in m))
+
